388905e8f16a6e01a2edd48e723b60545802b7d2
Wesley <wesley.pf@outlook.com>
2025-07-03 22:50:52 -0300

    Add middlewares

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 import { Response, Request,Router } from "express";
 import { UsersControllers } from "../controllers/userControllers";
+import { isUserIdValid } from "../middlewares/isIuser.middleware";
 
 export const routeList = Router()
 const usersControllers = new UsersControllers()
 
 routeList.get('/', usersControllers.getUsers )
-routeList.put('/:id', usersControllers.updateUsers )
-routeList.delete('/:id', usersControllers.deleteUser )
+routeList.put('/:id', isUserIdValid.execute, usersControllers.updateUsers )
+routeList.delete('/:id', isUserIdValid.execute, usersControllers.deleteUser )
 
 
 routeList.post('/', usersControllers.createUser)
