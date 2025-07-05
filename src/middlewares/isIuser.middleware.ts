8bcd25394e71a344652c3727d33f622663ba3ac4
Wesley <wesley.pf@outlook.com>
2025-07-04 22:47:54 -0300

    Add more configurations middlewares

--- src/middlewares/isIuser.middleware.ts
+++ src/middlewares/isIuser.middleware.ts
@@ -5,12 +5,20 @@ const prisma = new PrismaClient()
 
 export class isUserIdValid{
     static async execute (req:Request,res:Response, next:NextFunction){
-        const idUser = req.params.id
-        const getId = await prisma.user.findFirst()
+        const idUser = String(req.params.id)
+        const getId = await prisma.user.findFirst({where:{id:idUser}})
         if(idUser !==getId?.id){ 
             
         return res.status(404).json({message:"Not found"})
         }
         next()
     }
+
+    static async wakupServer (req:Request, res:Response,next:NextFunction){
+        setTimeout(()=>{
+            
+        },3000)
+    next()
+    }
+    
 }
